60327fb0f7cffdfb68be38aceb2f56c3509f6072
puffy <nagy.gergo@3pixel.hu>
2017-10-16 20:00:17 +0200

    elso jatszadozasok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  tomb = [1, 2, 3, 4, 5, 6, 7];
+
+  constructor() {
+    const alma = this.tomb.filter((param: number) => param === 2);
+    console.log(alma);
+    console.log(this.tomb);
+    // console.log(this.tomb.filter((param: number) => param === 2));
+    // console.log(this.tomb.map((param: number) => param * 2));
+    // console.log(this.tomb.reduce((x,y) => x + y));
+  }
 }
